dc57fc62a984bbbf2bff6f0601c1f277e6c4242d
группы стационара <st@itstep.lan>
2017-12-19 20:56:38 +0300

    19122017

---
+++ config/mongoose.js
@@ -0,0 +1,6 @@
+var mongoose = require ('mongoose');
+var config = require ('../config');
+
+mongoose.connect ('mongodb://localhost/kurs');
+
+module.exports = mongoose;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var Themas = require('../models/themas').Themas;
 
 /* GET home page. */
 router.get('/:id?', function(req, res, next) {
@@ -11,6 +12,32 @@ router.get('/:id?', function(req, res, next) {
 	//console.log(title);
   res.render('index', { title: index });
 });
+
+router.get('/add/:url/:name/:body?', function(req, res, next) {
+	//res.render('index',{title:'Express'});
+	if(req.params.name != ''){
+			var p_name = req.params.name;
+	}else{  var p_name = "name2";
+	}
+	if(req.params.url){
+			var p_url = req.params.url;
+	}else{  var p_url = "test";
+	}
+	if(req.params.body){
+			var p_body = req.params.body;
+	}else{  var p_body = "default body";
+	}
+	
+	var themas = new Themas({
+		url:p_url,
+		name:p_name,
+		body:p_body
+	});
+	themas.save(function(err,user,affected){
+		console.log('asdf');
+	});
+});
+
 router.post('/reg',function(req,res,next){
 	console.log(req.body.username);
 	console.log(req.body.password);
